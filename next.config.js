767e0124c2bc0e2c694f9ccc188b35dad2e2113e
Seth Mwebi <sethmwebi27@gmail.com>
2022-01-05 13:13:23 +0300

    day 3 build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
 	images: {
 		domains: ["links.papareact.com"]
+	},
+	env: {
+		mapbox_key: "pk.eyJ1Ijoic2V0aG13ZWJpIiwiYSI6ImNreTE5em0ybjA5ZXoyeXFuYmI3dGt4NjAifQ.yf5pWH-rRfO6hkf5Pl1euw"
 	}
 }
